a2e022725f8345e8f68ede8d9f81a0ef99487274
Arham Fatima <arham.fatima@gmail.com>
2019-09-20 00:42:24 +0500

    Fixed arguments count issue

--- defaultPackage/cmd_read_index.py
+++ defaultPackage/cmd_read_index.py
@@ -6,12 +6,12 @@ from nltk import SnowballStemmer
 start = timeit.default_timer()
 stemmer = SnowballStemmer("english")
 
-if len(sys.argv) == 4:
-    paramType = sys.argv[2]
+if len(sys.argv) == 3:
+    paramType = sys.argv[1]
 
     if paramType == '--term':
 
-        termName = stemmer.stem(sys.argv[3])
+        termName = stemmer.stem(sys.argv[2])
         termID = -1
 
         with open("termids.txt", "r", encoding="utf8") as file:
@@ -28,7 +28,7 @@ if len(sys.argv) == 4:
             with open("term_index.txt", "r", encoding="utf8") as file:
                 for line in file:
                     if len(line) > 0 and line.split()[0] == termID:
-                        print("Listing for term: " + sys.argv[3])
+                        print("Listing for term: " + sys.argv[2])
                         print("TERM_ID: " + str(termID))
                         print("Number of documents containing term: " + line.split()[2])
                         print("Term frequency in corpus: " + line.split()[1])
